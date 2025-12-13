2cebd6d297c3c0528f914e646071104df202a4fa
Isoheptane <chomodiso@outlook.com>
2025-12-13 15:57:10 +0800

    Add chat logging: Sticker, Animation, Audio, Document, Photo, Video, VideoNote, Voice, caption and text.

--- src/helper/log.rs
+++ src/helper/log.rs
@@ -78,23 +78,78 @@ impl<'a> Display for LogChatContent<'a> {
         let msg = self.0;
         // Supported message types
         if let Some(reply) = msg.reply_to_message.as_ref() {
-            write!(f, " > {}\n", LogChatSource(reply))?;
-            let content = LogChatContent(reply).to_string();
-            for line in content.lines() {
-                write!(f, " > {}\n", line)?;
-            }
+            writeln!(f, " > {}", LogChatSource(&reply))?;
+            chat_content_inner_helper(&reply, f, " > ")?;
         }
-        if let Some(sticker) = msg.sticker.as_ref() {
-            write!(f, "[Sticker")?;
-            if let Some(emoji) = sticker.emoji.as_ref() {
-                write!(f, " {}", emoji)?;
-            }
-            write!(f, "]")?;
-        }
-        if let Some(text) = msg.text.as_ref() {
-            write!(f, "{}", text)?;
-        }
+        chat_content_inner_helper(msg, f, "")?;
 
         Ok(())
     }
+}
+
+fn chat_content_inner_helper(msg :&Message, f: &mut std::fmt::Formatter<'_>, prefix: &str) -> std::fmt::Result {
+    if let Some(sticker) = msg.sticker.as_ref() {
+        write!(f, "{prefix}[Sticker")?;
+        if let Some(emoji) = sticker.emoji.as_ref() {
+            write!(f, " {}", emoji)?;
+        }
+        writeln!(f, "]")?;
+    }
+    if let Some(animation) = msg.animation.as_ref() {
+        write!(f, "{prefix}[Animation")?;
+        if let Some(name) = animation.file_name.as_ref() {
+            write!(f, " {}", name)?;
+        }
+        write!(f, " {}s", animation.duration)?;
+        writeln!(f, "]")?;
+    }
+    if let Some(audio) = msg.audio.as_ref() {
+        write!(f, "{prefix}[Audio")?;
+        if let Some(name) = audio.file_name.as_ref() {
+            write!(f, " {}", name)?;
+        }
+        write!(f, " {}s", audio.duration)?;
+        writeln!(f, "]")?;
+    }
+    if let Some(document) = msg.document.as_ref() {
+        write!(f, "{prefix}[Document")?;
+        if let Some(name) = document.file_name.as_ref() {
+            write!(f, " {}", name)?;
+        }
+        writeln!(f, "]")?;
+    }
+    if let Some(_) = msg.photo.as_ref() {
+        writeln!(f, "{prefix}[Photo]")?;
+        // There is few information about a photo
+    }
+    if let Some(video) = msg.video.as_ref() {
+        write!(f, "{prefix}[Video")?;
+        if let Some(name) = video.file_name.as_ref() {
+            write!(f, " {}", name)?;
+        }
+        write!(f, " {}s", video.duration)?;
+        writeln!(f, "]")?;
+    }
+    if let Some(video) = msg.video_note.as_ref() {
+        write!(f, "{prefix}[Video Note")?;
+        write!(f, " {}s", video.duration)?;
+        writeln!(f, "]")?;
+    }
+    if let Some(voice) = msg.voice.as_ref() {
+        write!(f, "{prefix}[Voice")?;
+        write!(f, " {}s", voice.duration)?;
+        writeln!(f, "]")?;
+    }
+    if let Some(caption) = msg.caption.as_ref() {
+        for line in caption.lines() {
+            writeln!(f,"{prefix}{line}")?;
+        }
+    }
+    if let Some(text) = msg.text.as_ref() {
+        for line in text.lines() {
+            writeln!(f,"{prefix}{line}")?;
+        }
+    }
+
+    Ok(())
 }
